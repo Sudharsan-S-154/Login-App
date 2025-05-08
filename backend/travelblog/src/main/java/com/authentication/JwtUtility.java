7b7ddc612807d571bacfefdfa3b72fcbde22daac
SUDHARSAN S <sudharsa8850@gmail.com>
2025-05-09 00:33:05 +0530

    added jwtutility class

---
+++ backend/travelblog/src/main/java/com/authentication/JwtUtility.java
@@ -0,0 +1,47 @@
+package com.authentication;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtility {
+
+	@Value("${jwtSecret}")
+	private String jwtSecret;
+
+	@Value("${expirationDate}")
+	private Integer expirationDate;
+
+	private String generateToken(Authentication auth) {
+		return Jwts.builder().setSubject(auth.getName()).setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + expirationDate))
+				.signWith(getKey(), SignatureAlgorithm.HS256).compact();
+	}
+
+	private Key getKey() {
+		return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+	}
+
+	private Boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private String getUserNameFromToken(String token) {
+		return Jwts.parserBuilder().setSigningKey(getKey()).build().parseClaimsJws(token).getBody().getSubject();
+	}
+
+}
