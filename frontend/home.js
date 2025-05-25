de1c493ccebbdf7c6618e1343919f2ac01ef9822
SUDHARSAN S <sudharsa8850@gmail.com>
2025-05-25 23:44:52 +0530

    partially added edit functionality

--- frontend/home.js
+++ frontend/home.js
@@ -64,49 +64,18 @@ addBlogForm.addEventListener("submit", async (event) => {
   const travelSpot = document.querySelector(".travelSpot").value;
   const experience = document.querySelector(".experience").value;
   const rating = document.querySelector(".rating").value;
-  console.log(`rating ${rating}`);
+
+  let requestObject = {
+    name: name,
+    phno: phno,
+    travelSpot: travelSpot,
+    experience: experience,
+    rating: rating,
+  };
 
   const token = localStorage.getItem("token");
   console.log(token);
-
-  const result = await fetch("http://localhost:8080/blog", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      name: name,
-      phno: phno,
-      travelSpot: travelSpot,
-      experience: experience,
-      rating: rating,
-    }),
-  });
-  const resultJson = await result.json();
-  console.log(resultJson);
-  debugger;
-  if (resultJson.data != null) {
-    success.textContent = "Blog is added successfully";
-    success.style.display = "block";
-    success.style.right = "30px";
-    setTimeout(() => {
-      success.style.right = "-500px";
-      success.style.display = "none";
-      location.reload();
-    }, 1500);
-    setTimeout(() => {
-      form1.style.display = "none";
-    }, 500);
-  } else {
-    failure.textContent = resultJson.error;
-    failure.style.right = "30px";
-    failure.style.display = "block";
-    setTimeout(() => {
-      failure.style.right = "-500px";
-      failure.style.display = "none";
-    }, 2000);
-  }
+  addOrEditBlog(requestObject, token);
 });
 
 // --------------------
@@ -142,7 +111,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     createBlog(resultJson);
     addStylesToBlog();
   }
-  
 
   myBlogButton.addEventListener("click", async () => {
     allBlogs.innerHTML = "";
@@ -153,12 +121,12 @@ document.addEventListener("DOMContentLoaded", async () => {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
-      }
+      },
     });
     const resultBlogJson = await resultBlog.json();
-    resultBlogJson.data.forEach((x)=>{
+    resultBlogJson.data.forEach((x) => {
       console.log(x);
-    })
+    });
     createBlog(resultBlogJson);
     addStylesToBlog();
   });
@@ -183,7 +151,24 @@ function createBlog(resultJson1) {
 
     console.log(rating);
 
-    allBlog.innerHTML = `<div class="buttonAndImg">
+    if (userDetails.role === "admin") {
+      // console.log("----------------->");
+      allBlog.innerHTML = `<div class="buttonAndImg">
+                            <img src= "travelblog.jpg"  alt="No content available" class="blogImg"> 
+                            <div class="allButton">
+                             <button class="viewBlog">View</button>
+                             <button class="editBlog">Edit</button>
+                             <button class="deleteBlog">Delete</button>
+                            </div>
+                          </div>
+                          <div class="nameAndRating">
+                           <h4 class="travelSpotName"> ${blog.travelSpot} </h4>   
+                           <div class="rating">${rating}</div>
+                          </div>
+                          
+                        `;
+    } else {
+      allBlog.innerHTML = `<div class="buttonAndImg">
                             <img src= "travelblog.jpg"  alt="No content available" class="blogImg"> 
                             <button class="viewBlog">View</button>
                           </div>
@@ -193,34 +178,52 @@ function createBlog(resultJson1) {
                           </div>
                           
                         `;
+    }
     allBlogs.append(allBlog);
   });
 }
 // -------------add styles to blog.
-function addStylesToBlog(){
-const buttonAndImgs = document.querySelectorAll(".buttonAndImg");
+function addStylesToBlog() {
+  const buttonAndImgs = document.querySelectorAll(".buttonAndImg");
   const blogImgs = document.querySelectorAll(".blogImg");
   const viewBlogs = document.querySelectorAll(".viewBlog");
+  const editBlogs = document.querySelectorAll(".editBlog");
+  const deleteBlogs = document.querySelectorAll(".deleteBlog");
+
   buttonAndImgs.forEach((buttonAndImg, index) => {
     const viewBlog = viewBlogs[index];
+    const editBlog = editBlogs[index];
+    const deleteBlog = deleteBlogs[index];
+    const allButton = [viewBlog, editBlog, deleteBlog];
     const blogImg = blogImgs[index];
+
+    allButton.forEach((button) => {
+      button.addEventListener("mouseenter", () => {
+        viewBlog.style.visibility = "visible";
+        editBlog.style.visibility = "visible";
+        deleteBlog.style.visibility = "visible";
+        blogImg.style.filter = "brightness(50%)";
+      });
+
+      button.addEventListener("mouseleave", () => {
+        viewBlog.style.visibility = "visible";
+        editBlog.style.visibility = "visible";
+        deleteBlog.style.visibility = "visible";
+        blogImg.style.filter = "brightness(50%)";
+      });
+    });
+
     buttonAndImg.addEventListener("mouseenter", () => {
       viewBlog.style.visibility = "visible";
+      editBlog.style.visibility = "visible";
+      deleteBlog.style.visibility = "visible";
       blogImg.style.filter = "brightness(50%)";
     });
     buttonAndImg.addEventListener("mouseout", () => {
       viewBlog.style.visibility = "hidden";
+      editBlog.style.visibility = "hidden";
+      deleteBlog.style.visibility = "hidden";
       blogImg.style.filter = "brightness(100%)";
-    });
-
-    viewBlog.addEventListener("mouseenter", () => {
-      viewBlog.style.visibility = "visible";
-      blogImg.style.filter = "brightness(50%)";
-    });
-
-    viewBlog.addEventListener("mouseleave", () => {
-      viewBlog.style.visibility = "visible";
-      blogImg.style.filter = "brightness(50%)";
     });
 
     const viewOneBlogMain = document.querySelector(".viewOneBlogMain");
@@ -244,10 +247,59 @@ const buttonAndImgs = document.querySelectorAll(".buttonAndImg");
       // main1.style.visibility="hidden";
     });
 
+    editBlog.addEventListener("click", async () => {
+      document.querySelector(".name").value = allBlogList[index].name;
+      document.querySelector(".phno").value = allBlogList[index].phno;
+      document.querySelector(".travelSpot").value =
+        allBlogList[index].travelSpot;
+      document.querySelector(".experience").value =
+        allBlogList[index].experience;
+      console.log(allBlogList[index].rating);
+      document.querySelector(".rating").value = allBlogList[index].rating;
+      main1.style.display = "none";
+      form1.style.display = "flex";
+    });
+
     const faCloseBlog = document.querySelector(".fa.fa-close.blog");
     faCloseBlog.addEventListener("click", () => {
       viewOneBlogMain.style.display = "none";
       main1.style.display = "inline-block";
     });
   });
+}
+
+//----------Method to add or edit blog
+async function addOrEditBlog(requestObject1, token1) {
+  const result = await fetch("http://localhost:8080/blog", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token1}`,
+    },
+    body: JSON.stringify(requestObject1),
+  });
+  const resultJson = await result.json();
+  console.log(resultJson);
+  debugger;
+  if (resultJson.data != null) {
+    success.textContent = "Blog is added successfully";
+    success.style.display = "block";
+    success.style.right = "30px";
+    setTimeout(() => {
+      success.style.right = "-500px";
+      success.style.display = "none";
+      location.reload();
+    }, 1500);
+    setTimeout(() => {
+      form1.style.display = "none";
+    }, 500);
+  } else {
+    failure.textContent = resultJson.error;
+    failure.style.right = "30px";
+    failure.style.display = "block";
+    setTimeout(() => {
+      failure.style.right = "-500px";
+      failure.style.display = "none";
+    }, 2000);
+  }
 }
